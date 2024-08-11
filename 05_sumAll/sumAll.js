fc17770255e6cb2fa53eb4149ca48179b8eb2179
bjornrohrer <barohrerdk@gmail.com>
2024-08-11 15:39:15 +0200

    Added the code to complete the project.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function (...args) {
+  let total = 0;
+  for (
+    i = Math.min.apply(null, args);
+    i <= Math.max.apply(null, args);
+    i += 1
+  ) {
+    total += i;
+  }
+  return total;
 };
 
 // Do not edit below this line
