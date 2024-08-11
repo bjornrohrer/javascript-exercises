83f143c0bad95a5e2ec8cb5877eba2c4d4f3a7ed
bjornrohrer <barohrerdk@gmail.com>
2024-08-11 15:53:57 +0200

    Removed skip from the next test.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,13 +1,17 @@
 const sumAll = function (...args) {
-  let total = 0;
-  for (
-    i = Math.min.apply(null, args);
-    i <= Math.max.apply(null, args);
-    i += 1
-  ) {
-    total += i;
+  if (args.some((num) => num < 0)) {
+    return "ERROR";
+  } else {
+    let total = 0;
+    for (
+      i = Math.min.apply(null, args);
+      i <= Math.max.apply(null, args);
+      i += 1
+    ) {
+      total += i;
+    }
+    return total;
   }
-  return total;
 };
 
 // Do not edit below this line
