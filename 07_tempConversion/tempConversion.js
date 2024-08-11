bcf12db97309cf114a375e32e281589a625f7501
bjornrohrer <barohrerdk@gmail.com>
2024-08-11 16:55:30 +0200

    Removed skips.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempFahrenheit) {
+  const celcius = (tempFahrenheit - 32) * (9 / 5);
+  return Math.round(celcius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempCelcius) {
+  const fahrenheit = (tempCelcius + 32) * (9 / 5);
+  return Math.round(fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
